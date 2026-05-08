064675b82d5dc75863dab8d7afbc5016b568ca81
John Kingoo <kingoojohn27@gmail.com>
2026-05-08 11:06:17 +0300

    Updated Article.js

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,12 @@
-function Article({ post }) {
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview }) {
   return (
-    <div>
-      <h2>{post.title}</h2>
-      <small>{post.date}</small>
-      <p>{post.preview}</p>
-    </div>
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+    </article>
   );
 }
 
