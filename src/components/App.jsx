ae48a20817e41830205a752847f28bcb01321b31
John Kingoo <kingoojohn27@gmail.com>
2026-05-08 09:04:21 +0300

    Fix blog import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import blog from "./blog";
+import blogData from "../data/blog"; // Adjusted path to standard data location
 
 import Header from "./Header";
 import About from "./About";
-import ArticleList from "./ArticleList";
+import ArticleList from "../components/ArticleList"; // Explicit path fix for components
 
 function App() {
   return (
     <div className="App">
-      <Header name={blog.name} />
+      <Header name={blogData.name} />
 
       <About
-        about={blog.about}
-        image={blog.image}
+        about={blogData.about}
+        image={blogData.image}
       />
 
-      <ArticleList posts={blog.posts} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
