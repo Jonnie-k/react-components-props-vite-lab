287aea6f55443b54000e6b135388a17efc5cbd4b
John Kingoo <kingoojohn27@gmail.com>
2026-05-08 08:34:37 +0300

    completed blog site lab

---
+++ src/components/blog.js
@@ -0,0 +1,20 @@
+const blog = {
+  name: "John's Blog",
+  about: "Welcome to my personal blog where I share my thoughts.",
+  posts: [
+    {
+      id: 1,
+      title: "My First Post",
+      date: "May 1, 2026",
+      preview: "This is my first blog post!"
+    },
+    {
+      id: 2,
+      title: "Learning React",
+      date: "May 2, 2026",
+      preview: "React is actually fun once you get it."
+    }
+  ]
+};
+
+export default blog;
